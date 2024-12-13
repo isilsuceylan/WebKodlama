4b01f0a9732e86917355a26dc47fa1a225e09c5f
Işılsu Ceylan <118476812+isilsuceylan@users.noreply.github.com>
2024-12-13 21:12:34 +0300

    .

--- php/index3.php
+++ php/index3.php
@@ -2,14 +2,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$mydb="bote24";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $mydb);
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-    
+
+
+
+// Sorgu
+$sql = "SELECT * FROM persons";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Her bir satırı döndür ve yazdır
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["PersonID"] . " - Name: " . $row["Firstname"] . " - Surname: " . $row["Lastname"] . " - Address " . $row["Address"] . <br>";
+    }
+} else {
+    echo "Sonuç bulunamadı.";
+}
 
 ?>
 
